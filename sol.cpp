a735665478bc2041718a6703c16755fc06300866
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-10-09 14:50:17 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -10,6 +10,11 @@ int main() {
 	while (tt--) {
 		long long n;
 		cin >> n;
+		// view the image in this repository for better understanding
+		// since negative numbers are included, we can simply include the negative
+		// equivalent of the numbers in the range from 'l' to 'r' (inclusive) and not
+		// include the negative equivalent of 'n' so that every pair would cancel each
+		// other out and leave 'n' behind as the final answer
 		cout << -(n - 1) << " " << n << '\n';
 	}
 	return 0;
